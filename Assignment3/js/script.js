17642456813be41276bbfd2c5c7c88b300be518b
Gaurav patil <100995820+gaurav9457@users.noreply.github.com>
2024-04-01 19:09:20 +0530

    Update script.js

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -1,12 +1,20 @@
-function validation() {
-    
-    let fname=document.getElementById("fname").value;
+ let fname=document.getElementById("fname").value;
     let lname=document.getElementById("lname").value;
-    let email=document.getElementById("email").value;
+   
     let mobile=document.getElementById("mobile").value;
     let dob=document.getElementById("dob").value;
     let  age=document.getElementById("age").value;
-    let country=document.getElementById("country").value;
+   //let country=document.getElementById("country").value;
+   let dobvalidate= /\d{1,2}\/\d{1,2}\/\d{4}/;
+function validation1() {
+    
+    let fname=document.getElementById("fname").value;
+    let lname=document.getElementById("lname").value;
+    
+    let mobile=document.getElementById("mobile").value;
+    let dob=document.getElementById("dob").value;
+    let  age=document.getElementById("age").value;
+    let city=document.getElementById("city").value;
     // let gender = document.querySelector('input[name="gender"]:checked').value;
 
     let checkbox = document.getElementsByName('Skills');
@@ -18,42 +26,34 @@ function validation() {
         }
     }
     
-    // console.log(fname," ",lname," ",email," ",mobile," ",dob," ",age," ",country," ",gender," ",selectedSkills);
-
+    
     let pattern=/^[A-Za-z]+$/;
     let num=/\d/;
-    let validemail=/^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
+   
     let count=/\d{10}$/;
-    let dobvalidate= /\d{1,2}\/\d{1,2}\/\d{2,4}/;
+    let dobvalidate= /\d{1,2}\/\d{1,2}\/\d{4}/;
     let ageCount=/\d{1,2}$/;
 
-    if (fname=="") {
+    if (fname=="" || num.test(fname)) {
         alert("first name is mandatory");
     } 
-    else if(num.test(fname)){
-        alert("First name not contain the numbers")
-    }
     else if(lname=="") {
         alert("last name is mandatory");
     }
     else if(num.test(lname)){
          alert("last name not contain numbers")
     }
-    else if(email==""){
-        alert("email id is mandatory");
-    }
-    else if (!validemail.test(email)) {
-        alert("Enter a proper email id");
-    }
+       
     else if(mobile==""){
         alert("Please enter mobile number");
     }
     else if(!count.test(mobile)){
         alert("Enter 10 digit mobile no");
     }
-    else if(!dobvalidate.test(dob)){
-        alert("Dob in dd/mm/yyyy");
-    }
+      // else if(!dobvalidate.test(dob)){
+        //alert("Dob in dd/mm/yyyy");
+    //}
+    	
     else  if(age==""){
         alert("Age is  mandatory");
     }
@@ -61,6 +61,36 @@ function validation() {
         alert("Enter 2 digit age");
     }
     else{
-        alert("submitted");
+        alert("Form submitted");
     }
+}
+
+function inputValid(){
+let displayMsg=document.getElementById("validMsg");
+let num=/\d/;
+
+}
+
+function dateValidate(){
+	let dob=document.getElementById("dob").value;
+	let dateObj = new Date();
+
+    if(!dobvalidate.test(dob)){
+    	  
+        alert("Dob in dd/mm/yyyy");
+    
+    	}
+	else if(dateobj.getFullYear()== Date.getFullYear()){
+	  alert("date must be less then current year");
+	}
+	else{
+	   alert("date accepted");
+	}
+
+}
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
